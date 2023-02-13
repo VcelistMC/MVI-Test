b434a397a05782b7d18ea3d85378a731822fcf77
Peter Atef <peter.atef@coformatique.com>
2023-02-13 11:39:08 +0200

    stuff

--- app/src/main/java/com/soft/myapplication/common/ViewEffect.kt
+++ app/src/main/java/com/soft/myapplication/common/ViewEffect.kt
@@ -1,6 +1,6 @@
 package com.soft.myapplication.common
 
-// empty interface to clearly define actions,
+// empty interface to clearly define view effects,
 // ViewActions are non persistent UI events such as:
 // 1. Show snack bar
 // 2. show dialog
